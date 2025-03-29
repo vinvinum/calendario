cfda35772cfddf1a4751598610e6fbd7c3310569
vinvinum <vin.vinum0@gmail.com>
2025-03-28 22:13:02 -0300

    adicionado interatividade do js

--- main.js
+++ main.js
@@ -2,6 +2,21 @@
 // utilizando função declarativa
 function colorirDia(){
     // bloco de variaveis (escopo de função)
-    let days = document.getElementById
-    ('day').value;
+    let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+
+    // Bloco de validação de informaçoes
+    if(!days){
+        alert ("Favor, informar um dia existente no calendario");
+    } else{
+        if((days > 0) && (days < 31)) {
+            let td = calendar.getElementsByTagName('td');
+            td.style.backgroundColor = color;
+        } else {
+            alert("Favor informar uma data constante no calendário")
+        }
+    }
+
+    var elementos = document.querySelectorAll('td');
 }
