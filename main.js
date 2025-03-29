622c5f8562d230fe357c75f1252700d0e5114e6c
vinvinum <vin.vinum0@gmail.com>
2025-03-28 21:56:57 -0300

    js criado

---
+++ main.js
@@ -0,0 +1,7 @@
+// Código para tornar calendário interativo
+// utilizando função declarativa
+function colorirDia(){
+    // bloco de variaveis (escopo de função)
+    let days = document.getElementById
+    ('day').value;
+}
